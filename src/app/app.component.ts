617d913f7b138faa5126e3b3680488a05fa2daa8
Inkant Awasthi <10158214+tim1016@users.noreply.github.com>
2020-06-23 23:52:00 -0500

    interval timer fromEvent observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { interval, timer, fromEvent } from "rxjs";
+import { shareReplay } from "rxjs/operators";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'app';
+  title = "app";
+  num = 0;
+  ngOnInit() {
+    const interval$ = timer(3000, 1000);
+
+    const sub = interval$.subscribe((val) => {
+      console.log(`Stream 1 : ${val}`);
+    });
+
+    const click$ = fromEvent(document, "click");
+
+    click$.subscribe(
+      (event) => {
+        console.log(event);
+      },
+      (error) => {
+        console.log(error);
+      },
+      () => {
+        console.log("completed");
+      }
+    );
+
+    setTimeout(() => {
+      sub.unsubscribe();
+    }, 16000);
+  }
 }
